a1a0dd26235f9d8095641482ec80d218c35deb94
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-08 12:21:35 +0200

    feat: implement server connection retries

---
+++ src/server/server.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import dotenv from 'dotenv';
+import { styleText as style } from 'node:util';
+import { DEF_SERVER_PORT } from '../common/constants';
+import { usersRouter } from '../router/usersRouter';
+import { startServer } from './server.utils';
+
+dotenv.config();
+
+const { BASE_PORT } = process.env;
+const port = Number(BASE_PORT || DEF_SERVER_PORT);
+const hostname = 'localhost';
+
+void startServer({ port, hostname, requestListener: usersRouter }, () => {
+  console.log(style('cyan', `\n🚀 Server running at http://${hostname}:${port}\n`));
+});
---
+++ src/server/server.utils.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { exec } from 'node:child_process';
+import type { RequestListener } from 'node:http';
+import http from 'node:http';
+import { promisify } from 'node:util';
+import { hasOwnKeys } from '../common/utils';
+
+const execAsync = promisify(exec);
+
+export const killServer = async (port: number): Promise<void> => {
+  try {
+    if (process.platform === 'win32') {
+      const { stdout } = await execAsync(`netstat -ano | findstr :${port}`);
+      const matches = stdout.match(/\d+$/gm) ?? [];
+
+      for (const pid of [...new Set(matches)]) {
+        await execAsync(`taskkill /f /pid ${pid}`);
+      }
+    } else {
+      await execAsync(`lsof -ti:${port} | xargs kill -9`);
+    }
+  } catch {
+    // empty;
+  }
+};
+
+type StartServerProps = {
+  port: number;
+  hostname: string;
+  requestListener: RequestListener;
+};
+
+export const startServer = async (
+  { port, hostname, requestListener }: StartServerProps,
+  onStart?: () => void,
+): Promise<void> => {
+  const server = http.createServer(requestListener);
+
+  await new Promise(resolve => {
+    server.on('error', async err => {
+      if (hasOwnKeys(err, 'code') && err.code === 'EADDRINUSE') {
+        console.log('⏳ Address in use, retrying...');
+        server.close();
+        await killServer(port);
+        server.listen(port, hostname);
+      }
+    });
+    server.on('listening', () => {
+      resolve(null);
+      onStart?.();
+    });
+    server.listen(port, hostname);
+  });
+};
